98b62fe76fae6e146c0caba12def02c853b602f3
Nelson Osacky <nelson@osacky.com>
2019-07-11 14:35:59 +0200

    Add incremental annotation processing support

--- assisted-inject-processor-dagger2/src/main/java/com/squareup/inject/assisted/dagger2/processor/AssistedInjectDagger2Processor.kt
+++ assisted-inject-processor-dagger2/src/main/java/com/squareup/inject/assisted/dagger2/processor/AssistedInjectDagger2Processor.kt
@@ -30,6 +30,8 @@ import com.squareup.inject.assisted.processor.internal.hasAnnotation
 import com.squareup.inject.assisted.processor.internal.toClassName
 import com.squareup.inject.assisted.processor.internal.toTypeName
 import com.squareup.javapoet.JavaFile
+import net.ltgt.gradle.incap.IncrementalAnnotationProcessor
+import net.ltgt.gradle.incap.IncrementalAnnotationProcessorType.AGGREGATING
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.Filer
 import javax.annotation.processing.Messager
@@ -44,6 +46,7 @@ import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
 import javax.tools.Diagnostic.Kind.ERROR
 
+@IncrementalAnnotationProcessor(AGGREGATING)
 @AutoService(Processor::class)
 class AssistedInjectDagger2Processor : AbstractProcessor() {
   override fun getSupportedSourceVersion() = SourceVersion.latest()
