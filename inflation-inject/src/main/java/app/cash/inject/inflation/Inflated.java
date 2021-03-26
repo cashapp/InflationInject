516ee70db0aa667f5251425909b0f79ed847743d
Jake Wharton <jw@squareup.com>
2021-03-26 17:18:39 -0400

    Add some quick docs to public types

--- inflation-inject/src/main/java/app/cash/inject/inflation/Inflated.java
+++ inflation-inject/src/main/java/app/cash/inject/inflation/Inflated.java
@@ -1,11 +1,20 @@
 package app.cash.inject.inflation;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
+/**
+ * Denotes the {@link Context} and/or {@link AttributeSet} parameters of an
+ * @link InflationInject @InflationInject}-annotated view constructor which are fullfilled by the
+ * layout inflater rather than the dependency graph.
+ *
+ * @see InflationInject
+ */
 @Retention(CLASS)
 @Target(PARAMETER)
 public @interface Inflated {
--- inflation-inject/src/main/java/app/cash/inject/inflation/InflationInject.java
+++ inflation-inject/src/main/java/app/cash/inject/inflation/InflationInject.java
@@ -1,11 +1,36 @@
 package app.cash.inject.inflation;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
+/**
+ * Denotes a view constructor which will be used for inflation. The normal {@link Context} and
+ * {@link AttributeSet} parameters should be marked with {@link Inflated @Inflated}. All other
+ * parameters will be fetched from the dependency graph.
+ * <p>
+ * For example:
+ * <pre><code>
+ * {@literal @}InflationInject
+ * public CustomView(
+ *   {@literal @}Inflated Context context,
+ *   {@literal @}Inflated AttributeSet attrs,
+ *   Picasso picasso
+ * ) {
+ *   super(context, attrs);
+ *   this.picasso = picasso;
+ * }
+ * </code></pre>
+ *
+ * The annotation processor will automatically include all {@code @InflationInject}-annotated types
+ * in a generated module that can be included in the graph.
+ *
+ * @see InflationModule
+ */
 @Retention(CLASS)
 @Target(CONSTRUCTOR)
 public @interface InflationInject {
